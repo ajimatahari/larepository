20f8e749b42b6e2f356530d6dc3fec22cfdc5cad
Malek Olabi <malek.olabi@gmail.com>
2018-02-14 12:26:24 +0100

    Add tests

---
+++ tests/TestCase.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Mola\Larepository\Tests;
+
+
+use Illuminate\Support\Facades\Config;
+use Mola\Larepository\LarepositoryServiceProvider;
+
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+
+        $app['config']->set('repository.repository_path', getenv('REPOSITORY_PATH'));
+        $app['config']->set('repository.model_path', getenv('MODEL_PATH'));
+        $app['config']->set('repository.contracts_path', getenv('CONTRACTS_PATH'));
+    }
+
+    /**
+     * Sets package providers for test-case.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [LarepositoryServiceProvider::class];
+    }
+}
