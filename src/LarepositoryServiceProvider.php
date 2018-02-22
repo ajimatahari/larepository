5d928ed28357cea53449795cadad7d3d7d6dff62
Malek Olabi <malek.olabi@gmail.com>
2018-02-22 19:50:20 +0100

    [Refactor] code reformat

--- src/LarepositoryServiceProvider.php
+++ src/LarepositoryServiceProvider.php
@@ -27,7 +27,8 @@ class LarepositoryServiceProvider extends ServiceProvider
         self::$packageLocation = dirname(__DIR__);
 
         $this->publishes([
-            self::$packageLocation . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'repository.php' => config_path('repository.php')
+            self::$packageLocation . DIRECTORY_SEPARATOR . 'config'
+            . DIRECTORY_SEPARATOR . 'repository.php' => config_path('repository.php')
         ], 'config');
 
         // Register commands
@@ -47,11 +48,15 @@ class LarepositoryServiceProvider extends ServiceProvider
     public function register()
     {
         $files = $this->app->make(Filesystem::class);
-        $provider = app_path('Providers' . DIRECTORY_SEPARATOR . RepositoryMakeCommand::$providerName . '.php');
+        $provider = app_path(
+            'Providers' . DIRECTORY_SEPARATOR . RepositoryMakeCommand::$providerName . '.php'
+        );
 
         // Register repository provider if created
         if ($files->exists($provider)) {
-            $this->app->register($this->getAppNamespace() . 'Providers\\' . RepositoryMakeCommand::$providerName);
+            $this->app->register(
+                $this->getAppNamespace() . 'Providers\\' . RepositoryMakeCommand::$providerName
+            );
         }
     }
 }
